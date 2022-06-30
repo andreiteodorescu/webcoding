b844cb1b7c74e7db51d6db5f939696b8f6461855
Andrei Teodorescu <andrei.teodorescu@avira.com>
2022-06-30 17:58:13 +0300

    about section desktop version and animations

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -4,16 +4,42 @@ const About = () => {
     return (
         <div className="container">
             <h4 className="general-title text-center">About</h4>
-            <div className="component-about-strength">
-                Top icon + text section
-            </div>
             <div className="component-about-info">
-                <div>
-                    Left photo + about text
-                </div>
-                <div>
-                    Right animated skills graph
-                </div>
+                <p className="about-summary">
+                    I'm a Front-End Developer for <a rel="noreferrer" href="https://www.nortonlifelock.com/us/en/" target="_blank">NortonLifeLock</a> in Bucharest, Romania.<br/>
+                    I love structure and order and I also stand for quality. I love spending time on fixing little details and optimizing web apps. Also I like working in a team, you'll learn faster and much more.
+                </p>
+                <figure className="about-image">
+                    <div className="about-image-shape">
+                        <img src={require("../../assets/images/andrei_teodorescu.jpg")} alt="Andrei teodorescu"/>
+                    </div>
+                </figure>
+                <ul className="skill-summary">
+                    <li className="skill-summary-item">
+                        <div className="skill-summary-fill" style={{width: '90%'}}>
+                            <div className="skill-summary-tag">HTML</div>
+                        </div>
+                        <span className="skill-summary-percentage">90%</span>
+                    </li>
+                    <li className="skill-summary-item">
+                        <div className="skill-summary-fill" style={{width: '90%'}}>
+                            <div className="skill-summary-tag">CSS</div>
+                        </div>
+                        <span className="skill-summary-percentage">90%</span>
+                    </li>
+                    <li className="skill-summary-item">
+                        <div className="skill-summary-fill" style={{width: '70%'}}>
+                            <div className="skill-summary-tag">Javascript</div>
+                        </div>
+                        <span className="skill-summary-percentage">70%</span>
+                    </li>
+                    <li className="skill-summary-item">
+                        <div className="skill-summary-fill" style={{width: '50%'}}>
+                            <div className="skill-summary-tag">React</div>
+                        </div>
+                        <span className="skill-summary-percentage">50%</span>
+                    </li>
+                </ul>
             </div>
         </div>
     )
