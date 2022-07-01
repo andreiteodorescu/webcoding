211bf92a404c782841c77837654dca05dbdd08eb
Andrei Teodorescu <andrei.teodorescu@avira.com>
2022-07-01 18:13:14 +0300

    portfolio desktop partial implementation with animations

--- src/components/portfolio-list/portfolio-list.component.jsx
+++ src/components/portfolio-list/portfolio-list.component.jsx
@@ -7,27 +7,30 @@ const PortfolioList = () => {
     return (
         <div className="container">
             <h4 className="general-title text-center">Clients</h4>
-            {PORTFOLIO_DATA.map(({id, name, tags, year}) => (
-                <InView className="component-portfolio-item" as="div" triggerOnce="true" rootMargin="-50px 0px" onChange={(inView, entry) => {
-                    if (inView) {
-                        entry.target.classList.add("in-view");
-                    }
-                }} key={'projkey-' + id}>
-                    <div className="component-portfolio-inner">
-                        <h4 className="component-portfolio-inner-title">
-                            {name}
-                        </h4>
-                        <ul className="list-unstyled project-tech">
-                            {tags.map(({id, tag_name}) => {
-                                return <li key={'tagkey-' + id }>{tag_name}</li>;
-                            })}
-                        </ul>
-                        <span className="project-year">
-                        {year}
-                    </span>
-                    </div>
-                </InView>
-            ))}
+            <ul className="component-portfolio-list">
+                {PORTFOLIO_DATA.map(({id, name, tags, year}) => (
+                    <InView className="component-portfolio-item" as="li" triggerOnce="true" rootMargin="-50px 0px" onChange={(inView, entry) => {
+                        if (inView) {
+                            entry.target.classList.add("in-view");
+                        }
+                    }} key={'projkey-' + id}>
+                        <div className="component-portfolio-inner">
+                            <h3 className="component-portfolio-inner-title">
+                                {name}
+                            </h3>
+                            <ul className="component-portfolio-inner-tech">
+                                {tags.map(({id, tag_name}) => {
+                                    return <li key={'tagkey-' + id }>{tag_name}</li>;
+                                })}
+                            </ul>
+                            <span className="component-portfolio-inner-year">
+                                {year}
+                            </span>
+                        </div>
+                        <div className="component-portfolio-animated-bg" aria-hidden="true"></div>
+                    </InView>
+                ))}
+            </ul>
         </div>
     )
 };
